7b920fc619683f7f4c2b7b706f1bc3407f87d395
u3u <qwq@qwq.cat>
2018-05-28 21:45:48 +0800

    chore(utils): remove unused functions

--- src/scripts/utils/index.ts
+++ src/scripts/utils/index.ts
@@ -1,22 +1,6 @@
-function hasRect(el: Element) {
-  const rect = el.getBoundingClientRect();
-  Object.values(rect).every(prop => prop !== 0);
-}
-
-function isVisible(el: Element) {
-  const { display, visibility } = getComputedStyle(el);
-  const visible = display !== 'none' && visibility !== 'hidden';
-  return visible && hasRect(el);
-}
-
+/* eslint-disable import/prefer-default-export */
 export function isMultiTextbox(el: Element) {
   const isTextArea = el.tagName === 'TEXTAREA';
   const isContentEditable = el.getAttribute('contenteditable');
   return isTextArea || isContentEditable;
-}
-
-export function getAllVisibleTextArea() {
-  return [...document.querySelectorAll('*')].filter(
-    (el: Element) => isMultiTextbox(el) && isVisible(el),
-  );
 }
