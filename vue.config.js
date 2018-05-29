57299c3b9733af34d362efeaefcbb3c27b1c5f88
u3u <qwq@qwq.cat>
2018-05-29 22:40:05 +0800

    chore(vue): remove useless eslint disable comment

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable strict */
 // https://github.com/vuejs/vue-cli/blob/dev/docs/config.md
 // https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-/* eslint-disable import/no-extraneous-dependencies */
 const path = require('path');
 
 module.exports = {
