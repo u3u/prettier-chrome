fc7644848982c714bbef7c5a8d8ffd0620a5fb80
u3u <qwq@qwq.cat>
2018-06-02 22:43:47 +0800

    chore(eslint): to disable unreasonable rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,9 +5,15 @@ module.exports = {
   },
   extends: ['plugin:vue/essential', '@vue/airbnb', '@vue/typescript'],
   rules: {
+    strict: ['off'],
     indent: ['off'],
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'import/extensions': ['off'],
+    'import/no-unresolved': ['off'],
+    'import/no-duplicates': ['off'],
+    'import/no-named-as-default': ['off'],
+    'import/no-named-as-default-member': ['off'],
     'import/no-extraneous-dependencies': ['off'],
     'function-paren-newline': ['off'],
     'class-methods-use-this': [
