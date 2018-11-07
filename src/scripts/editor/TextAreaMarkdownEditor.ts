76846fd8da18eb66eb56133c6e5f0efea5315090
u3u <qwq@qwq.cat>
2018-11-07 11:19:53 +0800

    fix(editor): incorrect value length

--- src/scripts/editor/TextAreaMarkdownEditor.ts
+++ src/scripts/editor/TextAreaMarkdownEditor.ts
@@ -12,7 +12,7 @@ export default class TextAreaMarkdownEditor implements MarkdownEditor {
   public setValue(value: string): void {
     // https://stackoverflow.com/questions/16195644/in-chrome-undo-does-not-work-properly-for-input-element-after-contents-changed-p
     this.editor.selectionStart = 0;
-    this.editor.selectionEnd = value.length;
+    this.editor.selectionEnd = this.editor.value.length;
     document.execCommand('insertText', false, value);
   }
 }
